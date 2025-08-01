e6b7275c063ea46d94af1b039e548a8742e89700
Padmakumar18 <padmakumar41759@gmail.com>
2025-08-01 12:54:11 +0530

    Fix issues in login and signup functionality

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
   );
---
+++ Frontend/my-app/src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/index.css";
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <div className="auth-bg min-h-screen ">
+        <div className="absolute inset-0 pointer-events-none z-0 bg-dots" />
+        <p className="text-white">Home page</p>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
